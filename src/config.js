8b12248bb980a08be50856a28556523f4f13d3ac
astral_huoba <1037536553@qq.com>
2025-07-05 08:43:08 +0800

    Delete_NoUse

--- src/config.js
+++ src/config.js
@@ -1,4 +1,22 @@
-// export const baseURL='http://172.20.10.8:5000';
-// export const baseURL='http://10.242.22.231:5000';
-// export const baseURL='http://192.168.171.23:5000';
+// export const baseURL1='http://172.20.10.8:5000';
+// export const baseURL2='http://10.242.22.231:5000';
+// export const baseURL3='http://192.168.171.23:5000';
+
 export const baseURL='http://192.168.171.110:5000';
+// 下面是本机ip地址查询
+// import os from 'os'
+// function getLocalIPv4() {
+//     const ips = []
+//     const networkInterfaces = os.networkInterfaces()
+
+//     Object.keys(networkInterfaces).forEach(interfaceName => {
+//         networkInterfaces[interfaceName].forEach(iface => {
+//             if (/ipv4/i.test(iface.family)) {
+//                 ips.push(iface.address)
+//             }
+//         })
+//     })
+
+//     return ips
+// }
+// console.log(getLocalIPv4());
